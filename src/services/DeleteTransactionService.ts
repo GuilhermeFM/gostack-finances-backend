ff2e315e573d3acea899f2f71ac7489c2b15dc1d
Guilherme F. Meira <guifrizzera2005@gmail.com>
2020-04-16 01:57:48 -0300

    Implementing transaction removal

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,21 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+import TransactionRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(transaction_id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+
+    const transaction = await transactionRepository.find({
+      where: { id: transaction_id },
+    });
+
+    if (!transaction || transaction.length === 0) {
+      throw new AppError('Transaction not found', 400);
+    }
+
+    await transactionRepository.remove(transaction);
   }
 }
 
