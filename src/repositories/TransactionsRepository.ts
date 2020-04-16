fc3983782a4d219166fff9f1928c798f990185d5
Guilherme F. Meira <guifrizzera2005@gmail.com>
2020-04-16 01:42:01 -0300

    Implementing create and list transaction scenarios

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,19 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const { total: total_income } = await this.createQueryBuilder('transactions')
+      .where('transactions.type = :type', { type: 'income' })
+      .select('SUM(COALESCE(transactions.value, 0))', 'total')
+      .getRawOne();
+
+    const { total: total_outcome } = await this.createQueryBuilder('transactions')
+      .where('transactions.type = :type', { type: 'outcome' })
+      .select('SUM(COALESCE(transactions.value, 0))', 'total')
+      .getRawOne();
+
+    const balance = total_income - total_outcome;
+
+    return { income: total_income, outcome: total_outcome, total: balance };
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,45 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+import Category from '../models/Category';
+
+interface TransactionParams {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute(params: TransactionParams): Promise<Transaction> {
+    const { title, value, type, category } = params;
+
+    const transactionRepository = getCustomRepository(TransactionRepository);
+    const balance = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && value > balance.total) {
+      throw new AppError('Insuficient funds.', 400);
+    }
+
+    const categoryRepository = getRepository(Category);
+    let related_category = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!related_category) {
+      related_category = categoryRepository.create({ title: category });
+      related_category = await categoryRepository.save(related_category);
+    }
+
+    const transaction_data = { title, value, type, category_id: related_category.id };
+    const transaction = transactionRepository.create(transaction_data);
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
